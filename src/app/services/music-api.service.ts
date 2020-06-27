4aeaa695f2c071ea595c0ca9476b3844363e0e0a
Hugo Vinicius <Hugo Vinicius@zapdesenv20.zap2.local>
2020-06-27 17:21:45 -0300

    Ajuste conflito

--- src/app/services/music-api.service.ts
+++ src/app/services/music-api.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-const API_KEY = "9c9825bd8831d74a3e5183de141b8fa8";
+const API_KEY = '9c9825bd8831d74a3e5183de141b8fa8';
 const baseUrl = (method, params) => `http://ws.audioscrobbler.com/2.0/?api_key=${API_KEY}&format=json&method=${method}&lang=pt${params}`;
 
 // methods
-const methodTagTopArtists = "tag.gettopartists";
-const methodArtistInfo = "artist.getinfo";
-const methodArtistSearch = "artist.search";
-const methodArtistTopAlbums = "artist.gettopalbums";
-const methodAlbumInfo = "album.getinfo";
+const methodTagTopArtists = 'tag.gettopartists';
+const methodArtistInfo = 'artist.getinfo';
+const methodArtistSearch = 'artist.search';
+const methodArtistTopAlbums = 'artist.gettopalbums';
+const methodAlbumInfo = 'album.getinfo';
 
 // params
 const paramTag = (genre) => `&tag=${genre}`;
 const paramArtist = (artist) => `&artist=${artist}`;
 const paramMbid = (id) => `&mbid=${id}`;
 const paramPage = (pageNumber) => `&page=${pageNumber}`;
-const paramLimit = "&limit=20";
+const paramLimit = '&limit=20';
 
 @Injectable({
   providedIn: 'root'
@@ -28,39 +28,39 @@ export class MusicApiService {
 
   // procura os artistas por um nome de gênero
   searchArtistsByGenre(genre: string, pageNum: number): Observable<any> {
-    let params = paramTag(genre) + paramLimit + paramPage(pageNum);
-    let url = baseUrl(methodTagTopArtists, params);
+    const params = paramTag(genre) + paramLimit + paramPage(pageNum);
+    const url = baseUrl(methodTagTopArtists, params);
     console.log(url);
     return this.http.get(url);
   }
 
   // recupera informações do artista pelo id
   getArtistInfo(idArtist: string): Observable<any> {
-    let params = paramMbid(idArtist);
-    let url = baseUrl(methodArtistInfo, params);
+    const params = paramMbid(idArtist);
+    const url = baseUrl(methodArtistInfo, params);
     console.log(url);
     return this.http.get(url);
   }
 
   // procura um artista pelo nome
   searchArtistsByName(nameArtist: string, pageNum: number): Observable<any> {
-    let params = paramArtist(nameArtist);
-    let url = baseUrl(methodArtistSearch, params);
+    const params = paramArtist(nameArtist);
+    const url = baseUrl(methodArtistSearch, params);
     console.log(url);
     return this.http.get(url);
   }
 
   // recupera os albuns de um artista
   getAlbumsByArtist(idArtist: string): Observable<any> {
-    let params = paramMbid(idArtist);
-    let url = baseUrl(methodArtistTopAlbums, params);
+    const params = paramMbid(idArtist);
+    const url = baseUrl(methodArtistTopAlbums, params);
     console.log(url);
     return this.http.get(url);
   }
 
   getAlbumByArtist(idAlbum: string): Observable<any> {
-    let params = paramMbid(idAlbum);
-    let url = baseUrl(methodAlbumInfo, params);
+    const params = paramMbid(idAlbum);
+    const url = baseUrl(methodAlbumInfo, params);
     console.log(url);
     return this.http.get(url);
   }
