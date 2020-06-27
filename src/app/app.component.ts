35fc52025cc33bf31015b24cd9dfbef001c84aa6
André Lordone <andgas@yahoo.com.br>
2020-06-27 16:53:41 -0300

    add novos serviços de acesso a API do Last.fm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,11 @@ export class AppComponent {
 
   constructor(private musicAPI: MusicApiService) {
     this.musicAPI.getArtistsByGenre("rock", 1).subscribe(json => console.log(json));
+    this.musicAPI.getArtistInfoByName("iron", 1).subscribe(json => console.log(json));
+    
     this.musicAPI.getArtistInfo("3798b104-01cb-484c-a3b0-56adc6399b80").subscribe(json => console.log(json));
     this.musicAPI.getAlbumsByArtist("3798b104-01cb-484c-a3b0-56adc6399b80").subscribe(json => console.log(json));
+    this.musicAPI.getAlbumByArtist("fa737b11-03c9-4f18-bcf9-a3ed4101c7fb").subscribe(json => console.log(json));
   }
 
 
--- src/app/music-api.service.ts
+++ src/app/music-api.service.ts
@@ -3,17 +3,21 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 const API_KEY = "9c9825bd8831d74a3e5183de141b8fa8";
+const baseUrl = (method, params) => `http://ws.audioscrobbler.com/2.0/?api_key=${API_KEY}&format=json&method=${method}&lang=pt${params}`;
 
 // methods
 const methodTagTopArtists = "tag.gettopartists";
 const methodArtistInfo = "artist.getinfo";
+const methodArtistSearch = "artist.search";
 const methodArtistTopAlbums = "artist.gettopalbums";
+const methodAlbumInfo = "album.getinfo";
 
-const findArtistGenre = (genre) => `&tag=${genre}`;
-const mbid = (id) => `&mbid=${id}`;
-const page = (pageNumber) => `&page=${pageNumber}`;
-const limit = "&limit=20";
-const baseUrl = (method, params) => `http://ws.audioscrobbler.com/2.0/?api_key=${API_KEY}&format=json&method=${method}&lang=pt${params}`;
+// params
+const paramTag = (genre) => `&tag=${genre}`;
+const paramArtist = (artist) => `&artist=${artist}`;
+const paramMbid = (id) => `&mbid=${id}`;
+const paramPage = (pageNumber) => `&page=${pageNumber}`;
+const paramLimit = "&limit=20";
 
 @Injectable({
   providedIn: 'root'
@@ -22,26 +26,41 @@ export class MusicApiService {
 
   constructor(private http: HttpClient) { }
 
-  // recupera os artistas por um nome de gênero
+  // procura os artistas por um nome de gênero
   getArtistsByGenre(genre: string, pageNum: number): Observable<any> {
-    let params = findArtistGenre(genre) + limit + page(pageNum);
+    let params = paramTag(genre) + paramLimit + paramPage(pageNum);
     let url = baseUrl(methodTagTopArtists, params);
     console.log(url);
     return this.http.get(url);
   }
 
-  // recupera informações do artista
-  getArtistInfo(id: string): Observable<any> {
-    let params = mbid(id);
+  // recupera informações do artista pelo id
+  getArtistInfo(idArtist: string): Observable<any> {
+    let params = paramMbid(idArtist);
     let url = baseUrl(methodArtistInfo, params);
     console.log(url);
     return this.http.get(url);
   }
 
+  // procura um artista pelo nome
+  getArtistInfoByName(nameArtist: string, pageNum: number): Observable<any> {
+    let params = paramArtist(nameArtist);
+    let url = baseUrl(methodArtistSearch, params);
+    console.log(url);
+    return this.http.get(url);
+  }
+
   // recupera os albuns de um artista
-  getAlbumsByArtist(id: string): Observable<any> {
-    let params = mbid(id);
+  getAlbumsByArtist(idArtist: string): Observable<any> {
+    let params = paramMbid(idArtist);
     let url = baseUrl(methodArtistTopAlbums, params);
+    console.log(url);
+    return this.http.get(url);
+  }
+
+  getAlbumByArtist(idAlbum: string): Observable<any> {
+    let params = paramMbid(idAlbum);
+    let url = baseUrl(methodAlbumInfo, params);
     console.log(url);
     return this.http.get(url);
   }
